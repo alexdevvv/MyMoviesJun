fd404133c69f20b9fb5535d2e720aebba05beee7
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2022-06-22 10:13:22 +0300

    downloading movies from the web

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -11,7 +11,6 @@ import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
